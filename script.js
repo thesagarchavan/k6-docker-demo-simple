b2c61e97b11f1baa18cc221b85df4355a1bc14ff
thesagarchavan <64186891+thesagarchavan@users.noreply.github.com>
2024-03-08 14:52:31 +0530

    To test workflow trigger

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ import { sleep, check } from 'k6';
 export default function() {
   const response = http.get('https://test.k6.io');
   check(response, {"Status is Ok(200)": (r) => r.status === 200})
-  sleep(1);
+  sleep(0.5);
 }
