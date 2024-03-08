ac348915988053b3ab1e771b4ed844c2fe6a26d4
thesagarchavan <64186891+thesagarchavan@users.noreply.github.com>
2024-03-08 14:06:06 +0530

    Tests script

---
+++ script.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export default function() {
+  const response = http.get('https://test.k6.io');
+  check(response, {"Status is Ok(200)": (r) => r.status === 200})
+  sleep(1);
+}
